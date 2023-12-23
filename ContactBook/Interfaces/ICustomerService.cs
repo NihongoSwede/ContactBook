a8fc4d92b8eb6493c005f9905366ecbddbe23d9c
NihongoSwede <mhedenborg18@gmail.com>
2023-12-23 13:01:32 +0100

    Fixed and error after last update

--- ContactBook/Interfaces/ICustomerService.cs
+++ ContactBook/Interfaces/ICustomerService.cs
@@ -1,17 +1,21 @@
 ﻿using ContactBook.Models.Responses;
-using System.Collections.Generic;
 
 namespace ContactBook.Interfaces
 {
     public interface ICustomerService
     {
-        bool Add(ICustomer customer);
-        IEnumerable<ICustomer> GetAll();
+        bool AddToList(ICustomer customer);
+        IEnumerable<ICustomer> GetAllFromList();
+        
+        ContactBookServiceResult DeleteCustomerByEmail(string email);
+       
+        ContactBookServiceResult UpdateCustomer(ICustomer updatedCustomer);
 
-        ContactBookServiceResult DeleteByEmail(string email);
+        void PrintCustomerByEmail(string email);
 
-        ContactBookServiceResult Update(ICustomer updatedCustomer);
+        
 
-        void PrintByEmail(string email);
+        
+
     }
 }
